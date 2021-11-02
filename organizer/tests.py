901bb6bc9277a525dc1a44aabd321cc2f2066c53
geetan-gandhe <80914912+geetan-gandhe@users.noreply.github.com>
2021-11-02 02:04:07 -0400

    Update tests.py

--- organizer/tests.py
+++ organizer/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class LoginTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        test_user = User.objects.create(username='tester')
+        test_user.set_password('a-27')
+        test_user.save()
+
+    def test_validate_correct(self):
+        c = Client()
+        correct_user = c.login(username ='tester', password = 'a-27')
+        self.assertTrue(correct_user)
+
+    def test_validate_logout(self):
+        c = Client()
+        c.login(username ='tester', password = 'a-27')
+        User = get_user_model()
+        user = User.objects.get(username = 'tester')
+        self.assertFalse(user.is_anonymous)
+        #c.logout()
+        #self.assertTrue(user.is_authenticated)
+        
