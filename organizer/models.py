57a973f42a39e5dd67042bb215e66b489e2a26fa
Chenelle Williams <cw4fk@virginia.edu>
2021-11-06 21:02:16 -0400

    to-do list basic

--- organizer/models.py
+++ organizer/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -26,3 +27,30 @@ class NotesUploadForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ('file','course',)
+
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Category(models.Model): # The Category table name that inherits models.Model
+	name = models.CharField(max_length=100) #Like a varchar
+
+	class Meta:
+		verbose_name = ("Category")
+		verbose_name_plural = ("Categories")
+
+	def __str__(self):
+		return self.name #name to be shown when called
+
+class TodoList(models.Model): #Todolist able name that inherits models.Model
+	title = models.CharField(max_length=250) # a varchar
+	content = models.TextField(blank=True) # a text field 
+	created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
+	due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
+	category = models.ForeignKey(Category,  on_delete=models.PROTECT, default="general") # a foreignkey
+
+	class Meta:
+		ordering = ["-created"] #ordering by the created field
+
+	def __str__(self):
+		return self.title #name to be shown when called
