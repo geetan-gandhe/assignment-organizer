09dbe973ba98e67d0d713ff229608a20b6120d7f
elenaxwang <89957553+elenaxwang@users.noreply.github.com>
2021-11-08 14:08:37 -0500

    more tests

--- organizer/tests.py
+++ organizer/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from organizer.models import Class,Notes
-
+from django.core.files import File
+from django.urls import reverse
+from organizer.views import ClassListView
+import mock
 
 class LoginTest(TestCase):
     def setUp(self):
@@ -25,7 +28,6 @@ class LoginTest(TestCase):
         #self.assertTrue(user.is_authenticated)
 
 #test_models
-
 class ClassTest(TestCase):
     @classmethod
     def setUp(cls):
@@ -36,10 +38,12 @@ class ClassTest(TestCase):
         class_name_test = test_class.__str__()
         #print(class_name_test)
         self.assertEqual(class_name_test,'CS2110')
+
+    def test_max_length(self):
+        test_class = Class.objects.create(class_name='CS2110')
         max_length = test_class._meta.get_field('class_name').max_length
         #print(max_length)
         self.assertEqual(max_length,100)
-
 
 
 
