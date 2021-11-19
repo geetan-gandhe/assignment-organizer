32a129ad2d561969eea7cb56d619bd95d5e7d62f
Connor Soh <connorsoh@gmail.com>
2021-11-19 18:26:57 -0500

    calendar shows up

--- organizer/views.py
+++ organizer/views.py
@@ -167,9 +167,6 @@ def index(request): #the index view
 
 	return render(request, "organizer/index.html", {"todos": todos, "categories":categories})
 
-def index2(request):
-    return HttpResponse('hello')
-
 class CalendarView(generic.ListView):
     model = Event
     template_name = 'organizer/calendar.html'
@@ -193,3 +190,16 @@ def get_date(req_day):
         year, month = (int(x) for x in req_day.split('-'))
         return date(year, month, day=1)
     return datetime.datetime.today()
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
