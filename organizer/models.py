a68e4efd2bb5140b3884096e2cbf36766f1a927c
Connor Soh <connorsoh@gmail.com>
2021-11-19 19:17:48 -0500

    events

--- organizer/models.py
+++ organizer/models.py
@@ -72,3 +72,7 @@ class Event(models.Model):
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    @property
+    def get_html_url(self):
+        url = reverse('cal:event_edit', args=(self.id,))
+        return f'<a href="{url}"> {self.title} </a>'
