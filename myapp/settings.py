20688219350ba58bcf29fb5b1429e38e481c863e
Joshua Goedert <jag5mms@virginia.edu>
2021-11-30 14:46:53 -0500

    fixing site id

--- myapp/settings.py
+++ myapp/settings.py
@@ -177,7 +177,7 @@ SENDGRID_PASSWORD='SENDGRID_PASSWORD'
 SENDGRID_USERNAME='SENDGRID_USERNAME'
 
 #oiqnqjanlnculncw
-SITE_ID = 4 #ID is 3 on prod! make sure to change before commits (4 for Chenelle's local)
+SITE_ID = 3 #ID is 3 on prod! make sure to change before commits (4 for Chenelle's local)
 
 LOGIN_REDIRECT_URL = '/organizer/home/'
 LOGOUT_REDIRECT_URL = '/organizer/home/'
